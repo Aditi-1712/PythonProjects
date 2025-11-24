429a251ef3fbf023bf5f3f29f3ceac05a741716c
Aditi Sharma <65812264+Aditi-1712@users.noreply.github.com>
2025-11-24 12:05:59 -0500

    Create capture.py

---
+++ ImgProcessing/video/capture.py
@@ -0,0 +1,44 @@
+import cv2, time
+
+firstFrame = None;
+
+video = cv2.VideoCapture(0)
+
+while True:
+    
+    check, frame = video.read()
+
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY);
+    gray = cv2.GaussianBlur(gray, (21,21),0);
+
+    if firstFrame is None:
+        firstFrame = gray;
+        continue;
+
+    deltaFrame = cv2.absdiff(firstFrame, gray);
+
+    threshFrame = cv2.threshold(deltaFrame, 30,255, cv2.THRESH_BINARY)[1];
+    threshFrame = cv2.dilate(threshFrame, None, iterations = 2);
+
+    (cnts,_) = cv2.findContours(threshFrame.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE);
+
+    for contour in cnts:
+        if cv2.contourArea(contour) < 1000:
+            continue
+
+        (x,y,w,h) = cv2.boundingRect(contour);
+        cv2.rectangle(frame, (x,y), (x+w, y+h), (0,255,0), 3);
+
+    cv2.imshow("Gray Frame",gray)
+    cv2.imshow("Delta Frame",deltaFrame);
+    cv2.imshow("Threshold Frame", threshFrame);
+    cv2.imshow("Color Frame", frame)
+
+    key = cv2.waitKey(1);
+    if key==ord('q'):
+        break;
+
+print(gray);
+print(deltaFrame)
+video.release()
+cv2.destroyAllWindows();
